30b4608dcfc60291c8da0cbc6d0216f81e4e97f0
Evanicio <evanicio29@gmail.com>
2023-08-19 18:11:54 -0300

    Selecionando sua pizza

--- script.js
+++ script.js
@@ -79,3 +79,12 @@ function closeModal () {
     c('.pizzaInfo--qt').innerHTML = modalQt;
     
   });
+
+  // Selecionando o tamanho da pizza.
+
+cs('.pizzaInfo--size').forEach((size , sizeIndex) => {
+	size.addEventListener('click', (e) => {
+    c('.pizzaInfo--size.selected').classList.remove('selected');
+size.classList.add('selected');
+  });
+});
